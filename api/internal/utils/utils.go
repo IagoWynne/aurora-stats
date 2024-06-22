9bd1347a6fd88a3b3d53d7453b7266a3d04652f1
James Richardson <iagowynne@gmail.com>
2024-06-22 16:23:44 +0100

    Time data is stripped from vibecheck datetime

--- api/internal/utils/utils.go
+++ api/internal/utils/utils.go
@@ -1,5 +1,7 @@
 package utils
 
+import "time"
+
 func MapArray[T any, U any](results []U, mapFn func(U) T) []T {
 	var output []T
 
@@ -8,4 +10,8 @@ func MapArray[T any, U any](results []U, mapFn func(U) T) []T {
 	}
 
 	return output
+}
+
+func TruncateToDay(t time.Time) time.Time {
+	return time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, t.Location())
 }
